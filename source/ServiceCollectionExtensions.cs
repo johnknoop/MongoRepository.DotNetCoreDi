efb8ef73e88f1a6b0c15bab6d7e79530d9f3e5e3
John Knoop <knoopjohn@gmail.com>
2019-04-19 19:14:16 +0200

    Added source files

---
+++ source/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace JohnKnoop.MongoRepository.DotNetCoreDi
+{
+	public delegate string ResolveTenantKey();
+
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddRepositories(this IServiceCollection services, IMongoClient mongoClient)
+		{
+			foreach (var entityType in MongoRepository.GetMappedTypes())
+			{
+				var genericRepositoryType = typeof(IRepository<>).MakeGenericType(entityType);
+
+				services.AddTransient(genericRepositoryType, provider =>
+				{
+					var tenantKey = provider.GetService<ResolveTenantKey>()();
+					var getRepositoryMethod = typeof(MongoConfiguration).GetMethod(nameof(MongoConfiguration.GetRepository));
+					var getRepositoryMethodGeneric = getRepositoryMethod.MakeGenericMethod(entityType);
+
+					return getRepositoryMethodGeneric.Invoke(null, new object[] { mongoClient, tenantKey });
+				});	
+			}
+
+			return services;
+		}
+	}
+}
